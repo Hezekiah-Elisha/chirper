be4e709e1b1da1ebe2e78b665021a1f90ba73975
Hezekiah Elisha <elishahezekiah903@gmail.com>
2026-05-08 00:25:59 +0300

    Add Chirp model, migration, and update ChirpController to fetch chirps from database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,22 +3,24 @@
         Welcome
     </x-slot:title>
     <div class="max-w-2xl mx-auto">
-            @foreach ($chirps as $chirp)
+            @forelse ($chirps as $chirp)
                 <div class="card bg-base-100 shadow mt-8">
                     <div class="card-body">
                         <div>
                             <div class="font-semibold">
-                                {{ $chirp['author'] }}
+                                {{ $chirp->user->name ?? 'Unknown User' }}
                             </div>
                             <p class="mt-1">
-                                {{ $chirp['message'] }}
+                                {{ $chirp->message->limit(244) }}
                             </p>
                             <p class="text-sm text-gray-500 mt-2">
-                                {{ $chirp['time'] }}
+                                {{ $chirp->created_at->diffForHumans() }}
                             </p>
                         </div>
                     </div>
                 </div>
-            @endforeach
+            @empty
+                <p class="text-center text-gray-500">No chirps to display.</p>
+            @endforelse
         </div>
 </x-layout>
